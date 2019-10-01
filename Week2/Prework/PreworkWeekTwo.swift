2eafc524fee2754f66a66f092cf62cb80034c520
Janet <44039641+jaurinu@users.noreply.github.com>
2019-09-30 20:50:12 -0500

    Create PreworkWeekTwo.swift

---
+++ Week2/Prework/PreworkWeekTwo.swift
@@ -0,0 +1,23 @@
+import Foundation
+print("Hello World")
+
+//Arrays are collections of values that are stored as a
+//single value
+
+let john = "John Lenon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Star"
+
+let beatles = [john, paul, george, ringo]
+//for read value from an array writing a number inside brackets
+print(beatles[0])
+
+//SETS are collections of values just like arrays, except they have
+//two differences; items are not stored in any order and no item
+//can appear twice
+
+let colors = Set(["red", "green", "blue"])
+print(colors)
+
+//TUPLES 
